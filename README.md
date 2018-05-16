7466282a37b8b9433316cbc36b06811d1bef132f
lauraitp <laura.tabares@correo.icesi.edu.co>
2018-05-16 08:36:54 -0500

    Update file stat.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # so-microservice
+
+**nombre:** Laura Isabella Tabares Perez
+**correo:** laura.tabares@correo.icesi.edu.co
+
--- op_stats/stats.py
+++ op_stats/stats.py
@@ -5,4 +5,7 @@ class Stats():
   @classmethod
   def get_cpu_percent(cls):
     cpu_percent = psutil.cpu_percent()
+   # swap_memory=psutil.swap_memory()
+   # disk_usage= psutil.disk_usage() 
     return cpu_percent
+	
